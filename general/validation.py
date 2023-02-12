8f65b4154ffa5cbab41a3fb43700202af2fd5c14
Nathaniel Mitrani <nathanielmitrani@gmail.com>
2023-02-12 23:53:13 +0100

    Update validation.py

--- general/validation.py
+++ general/validation.py
@@ -7,6 +7,7 @@ import networkx as nx
 
 from xgb_model import preprocess_data, make_predictions
 from src.graph_utils import compareGraphs
+from general.data_generation import graph_to_df
 
 df = pd.read_csv('../data/graph_train.csv')
 print(df[['graph_id', 'benchmark']].head())
@@ -31,7 +32,7 @@ classifier.load_model('xgb.bin')
 #####
 # Auxiliary functions 
 #####
-def bfs_on_edges(g: nx.Graph, edge: list|tuple, depth_limit) -> list:
+def bfs_on_edges(g: nx.Graph, edge: list or tuple, depth_limit) -> list:
     bfs_edges = {edge}
     sp = dict(nx.all_pairs_shortest_path_length(g))
     for e in nx.edge_bfs(g, edge[0]):
@@ -120,14 +121,31 @@ def relax_block(g: nx.Graph, data: pd.DataFrame, draw_f, model: XGBClassifier, d
     # returns (num_crossings, aspect_ratio, mean_crossing_angle, pseudo_vertex_resolution, mean_angular_resolution, mean_edge_length, edge_length_variance)
     return compareGraphs(g, g, draw_f(g), draw_f(g2), show=False)
 
+def relax_and_recompute(graph:nx.Graph,idx_graph:int,draw_f,model:XGBClassifier,data:pd.DataFrame,k:int=3):
+    """Relax 1 edge -> recompute -> relax 1 edge -> recompute -> ... k times"""
+    X,y = preprocess_data(data)
+    proba = make_predictions(model,X,ret_proba=True)
+    removed_edges = []
+    pos0 = draw_f(graph)
+    pos1 = pos0
+    for i in range(k):
+        max_proba_idx = np.argmax(proba)
+        max_proba_edge = list(graph.edges)[max_proba_idx]
+        removed_edges.append(max_proba_edge)
 
+        g2 = graph.copy()
+        g2.remove_edges_from([max_proba_edge])
+        pos1 = draw_f(g2,pos=pos1)
 
+        data = graph_to_df(g2,idx_graph,draw_f,bench='Test')
+        proba = make_predictions(model,data,ret_proba=True)
+    return compareGraphs(graph,graph,pos0,pos1,show=False,rmedges=removed_edges)
 #####
 # Evaluation functions 
 #####
 
 def eval_relax_one(model: XGBClassifier, df: pd.Dataframe, graphid2src: dict, draw_f):
-    """Evaluate the method relax_one
-    
+    """Evaluate the method relax_one"""
+    ...
 
 
